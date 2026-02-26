85f47dd547b76650e38a5fbc7d5d4889a3985c95
Simple <leikai9321@gmail.com>
2026-02-26 23:55:19 +0800

    Optimize system standard catalog

--- src/storage.rs
+++ src/storage.rs
@@ -1,23 +1,33 @@
 use crate::model::Todo;
 use anyhow::Result;
+use dirs::data_dir;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-const FILE_PATH: &str = "todos.json";
+fn get_file_path() -> PathBuf {
+    let mut path = data_dir().expect("无法获取数据目录");
+    path.push("todo-cli");
+    std::fs::create_dir_all(&path).ok();
+    path.push("todos.json");
+    path
+}
 
 pub fn load() -> Result<Vec<Todo>> {
-    if !Path::new(FILE_PATH).exists() {
+    let file_path = get_file_path();
+    //dbg!(&file_path);
+    if !file_path.exists() {
         return Ok(vec![]);
     }
 
-    let data = fs::read_to_string(FILE_PATH)?;
+    let data = fs::read_to_string(file_path)?;
     let todos = serde_json::from_str(&data)?;
     Ok(todos)
 }
 
 pub fn save(todos: &Vec<Todo>) -> Result<()> {
+    let file_path = get_file_path();
     let data = serde_json::to_string_pretty(todos)?;
-    fs::write(FILE_PATH, data)?;
+    fs::write(file_path, data)?;
     Ok(())
 }
 
